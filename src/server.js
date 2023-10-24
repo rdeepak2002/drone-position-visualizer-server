e26ce0e9e45482a4fbb822a6d82acb5fa7f88ed2
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-10-23 21:01:24 -0400

    More logging

--- src/server.js
+++ src/server.js
@@ -70,7 +70,7 @@ function sendCompetitionData(payload, cb) {
     if (transmitDataToCompetitionServer) {
         console.debug("Transmitting of competition server data enabled");
         const url = "https://a1m2ll84zs7epx-ats.iot.us-east-2.amazonaws.com:8443/topics/adaptitrace";
-        console.debug("Sending payload to competition server", payload);
+        console.debug("Sending payload to competition server body", payload?.body || "No body");
         request({
             method: "POST",
             uri: url,
